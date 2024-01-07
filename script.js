c393d01c4156bfdb868d3ed40d727f66ec2e5316
Karan Sayambar <86702398+Karansayambar@users.noreply.github.com>
2024-01-07 11:30:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+function updateSize(){
+	const widthElement = document.getElementById("width");
+	const heightElement = document.getElementById("height");
+
+	widthElement.textContent = window.innerWidth;
+	heightElement.textContent = window.innerHeight;
+}
+
+window.addEventListener("load", updateSize);
+window.addEventListener("resize", updateSize);
